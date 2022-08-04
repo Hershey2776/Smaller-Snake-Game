c1c45d5a81b11ffed2e13745c687d6cb71fc461a
hershey <hershey2776@gmail.com>
2022-08-05 05:26:07 +0530

    First Commit

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const CANVAS_SIZE = [400, 400];
+const SNAKE_START = [
+  [8, 7],
+  [8, 8],
+];
+const APPLE_START = [4, 4];
+const SCALE = 27;
+const SPEED = 103;
+const DIRECTIONS = {
+  38: [0, -1], // up
+  40: [0, 1], // down
+  37: [-1, 0], // left
+  39: [1, 0], // right
+};
+
+export { CANVAS_SIZE, SNAKE_START, APPLE_START, SCALE, SPEED, DIRECTIONS };
